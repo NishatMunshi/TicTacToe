d22f54e6dfc287fdcd981f7ba8d8f1ba7c25a9f0
Nishat <nishatmunshi9@gmail.com>
2023-05-13 13:36:38 +0530

    modified:   Makefile 	modified:   headers/game_objects.hpp 	modified:   src/main.cpp

--- headers/game_objects.hpp
+++ headers/game_objects.hpp
@@ -1,29 +1,90 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#ifndef WINDOW_DIMENSION
+#define WINDOW_DIMENSION 810.f
+#endif
 class Board
 {
 private:
-    sf::RectangleShape m_horizontalDivider, m_verticalDivider;
+    sf::RectangleShape m_horizontalDividers[4], m_verticalDividers[4];
+    float m_dividerThickness = 30.f;
 
 public:
+    Board(void)
+    {
+        int i = 0;
+        for (auto &divider : m_horizontalDividers)
+        {
+            divider.setSize({WINDOW_DIMENSION, m_dividerThickness});
+            divider.setPosition({0.f, i * (WINDOW_DIMENSION - m_dividerThickness) / 3});
+            i++;
+        }
+        int j = 0;
+        for (auto &divider : m_verticalDividers)
+        {
+            divider.setSize({m_dividerThickness, WINDOW_DIMENSION});
+            divider.setPosition({j * (WINDOW_DIMENSION - m_dividerThickness) / 3, 0.f});
+            j++;
+        }
+    }
+
+public: // inside loop
     void draw(sf::RenderWindow &_window)
     {
-        _window.draw(m_horizontalDivider);
-        _window.draw(m_verticalDivider);
+        for (auto &divider : m_horizontalDividers)
+        {
+            _window.draw(divider);
+        }
+        for (auto &divider : m_verticalDividers)
+        {
+            _window.draw(divider);
+        }
     }
 };
 class Cross
 {
 private:
-    const float m_armLength, m_armWidth;
-
-private:
+    const unsigned m_rowIndex, m_columnIndex;
+    const float m_armLength = (WINDOW_DIMENSION / 3.f - 120.f) * 1.414f, m_armWidth = 30.f;
     sf::RectangleShape m_frontIncliningArm{{m_armLength, m_armWidth}}, m_backIncliningArm{{m_armLength, m_armWidth}};
 
 public:
+    Cross(const unsigned _rowIndex, const unsigned _columnIndex)
+        : m_rowIndex(_rowIndex), m_columnIndex(_columnIndex)
+    {
+        m_frontIncliningArm.setRotation(135.f);
+        m_backIncliningArm.setRotation(45.f);
+        m_frontIncliningArm.setFillColor(sf::Color::Red);
+        m_backIncliningArm.setFillColor(sf::Color::Red);
+        m_frontIncliningArm.setPosition({m_columnIndex * (WINDOW_DIMENSION - 30.f) / 3.f + (WINDOW_DIMENSION - 30.f) / 3.f - 30.f, m_rowIndex * (WINDOW_DIMENSION - 30.f) / 3.f + 80.f});
+        m_backIncliningArm.setPosition({m_columnIndex * (WINDOW_DIMENSION - 30.f) / 3.f + 85.f, m_rowIndex * (WINDOW_DIMENSION - 30.f) / 3.f + 60.f});
+    }
     void draw(sf::RenderWindow &_window)
     {
         _window.draw(m_frontIncliningArm);
         _window.draw(m_backIncliningArm);
+    }
+};
+
+class Dot
+{
+private:
+    const unsigned m_rowIndex, m_columnIndex;
+    const float m_radius = WINDOW_DIMENSION / 6 - 75;
+    const float m_thickness = 30.f;
+    sf::CircleShape m_shape{m_radius};
+
+public:
+    Dot(const unsigned _rowIndex, const unsigned _columnIndex)
+        : m_rowIndex(_rowIndex), m_columnIndex(_columnIndex)
+    {
+        m_shape.setFillColor(sf::Color::Black);
+        m_shape.setOutlineThickness(m_thickness);
+        m_shape.setOutlineColor(sf::Color::Yellow);
+        m_shape.setPosition({m_columnIndex * (WINDOW_DIMENSION - 30.f) / 3.f + 85.f, m_rowIndex * (WINDOW_DIMENSION - 30.f) / 3.f + 85.f});
+    }
+    void draw(sf::RenderWindow &_window)
+    {
+        _window.draw(m_shape);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,33 @@
-#include<headers/game_objects.hpp>
-int main()
+#include <SFML/Graphics.hpp>
+#include <headers/game_objects.hpp>
+#ifndef WINDOW_DIMENSION
+#define WINDOW_DIMENSION 800.f
+#endif
+int main(int argc, char const *argv[])
 {
+    // init the window
+    sf::RenderWindow window(sf::VideoMode(WINDOW_DIMENSION, WINDOW_DIMENSION), "Tic Tac Toe");
+    window.setFramerateLimit(1);
+
+    Board board;
+    Dot dot(2, 2);
+    Cross cross(1,2);
+
+    while (window.isOpen())
+    {
+        // event check
+        sf::Event _event;
+        while (window.pollEvent(_event))
+        {
+            if (_event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        board.draw(window);
+        dot.draw(window);
+        cross.draw(window);
+        window.display();
+    }
     return 0;
 }
